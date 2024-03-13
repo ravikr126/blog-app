b825f9ee2c62efa30ac89d8db8417137e5d9a256
Ravi Kumar <ravi14577@gmail.com>
2024-03-13 15:45:00 +0530

    color and error change

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className='footer py-4 bg-purple'>
         <div className='container'>
             <div className='footer-content text-white text-center'>
-                <p className='copyright-text font-rubik fw-4 ls-1'>©2023 CopyRight. All rights reserved.</p>
+                <p className='copyright-text font-rubik fw-4 ls-1'>©2024 CopyRight. All rights reserved.</p>
             </div>
         </div>
     </footer>
